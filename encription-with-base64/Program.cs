41fed3e6c34e17b121d8d906e3ef3dcc0e76c657
doni <you@example.com>
2024-02-04 09:50:02 +0500

    new Project: encription of photo with Base64

---
+++ encription-with-base64/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Encription 
+
+{
+    class Programm 
+    
+    {
+        static void Main(string[] args) 
+        {
+            string fPath = @"D:\gwagen.png";
+            byte[] bytes = File.ReadAllBytes(fPath);
+            string file = Convert.ToBase64String(bytes);
+            System.Console.WriteLine(file);
+            // System.Console.WriteLine("converting Base64 to string ...\n" + file);
+            System.Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
